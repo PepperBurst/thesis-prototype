eba849888e65e6623eb55cccbb68ff87d4b4f663
Treestan Protector <mizzukiprime@gmail.com>
2018-12-12 18:51:11 +0800

    Cleaned code Added looping for connection faults Made 3 variables global (hi, temp, rh); Added releaseMist() sequnce function Adjusted getMotion() sequence function Added more info on serial monitor

--- thesis-prototype/thesis-prototype.ino
+++ thesis-prototype/thesis-prototype.ino
@@ -1,17 +1,28 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include "DHTesp.h"
 
 #define dhtPin D1
 #define pirPin D2
+#define outPin D3
 #define myPeriodic 15
-
+//APIKEY for Channel 1 = 459JH64HVXZMSZQJ
+//APIKEY for Channel 2 = N4CZSPBXEJF4U16D
 const char* server = "api.thingspeak.com";
-const char* MY_SSID = "PLDTMyDSL";
-const char* MY_PWD = "pldtwifiD6346";
-String apiKey = "19FK2MVUXKY4UR3N";
+String apiKey = "N4CZSPBXEJF4U16D"; // set Write API Key
 int sent = 0;
+bool delayForMist = false;
+int countDelay = 0;
+String fieldHI = "&field1=";
+String fieldTemp = "&field2=";
+String fieldRH = "&field3=";
+String moduleName = "Module 2";     //set Module #
+float hi = 0;
+float temp = 0;
+float rh = 0;
 
 DHTesp dht;
+ESP8266WiFiMulti wifiMulti;
 
 void setup() {
   Serial.begin(115200);
@@ -20,80 +31,104 @@ void setup() {
   Serial.println(thisBoard);
   dht.setup(dhtPin, DHTesp::DHT11); // Connect DHT sensor to GPIO 5
   pinMode(pirPin, INPUT);
+  pinMode(outPin, OUTPUT);
+  digitalWrite(outPin, 1);
+  connectWifi();
+  Serial.println("Swarm of Automatic Misting Systems with Android Application");
+  Serial.println(moduleName);
+  Serial.println();
 }
 
 void loop() {
-  Serial.print("Heat Index:\t");
-  float heatInd = getHeatIndex();
-  Serial.println(heatInd, 2);
-  if(heatInd>33){
-    Serial.println("Heat index above threshold, detecting motion...");
-    if(getMotion()){
-      Serial.println("Motion detected, releasing mist");
-    }else{
-      Serial.print("No motion detected");
+  while(wifiMulti.run() == WL_CONNECTED){
+    getReadings();
+    if(countDelay > 1){
+      countDelay = 0;
+      delayForMist = false;
     }
+    if(!delayForMist){
+      if(hi>32 && rh<90){
+        Serial.println("Heat index is above threshold, Relative Humidity is in valid range");
+        if(getMotion()){
+          Serial.println("Motion detected, releasing mist...");
+  //        digitalWrite(outPin, 0);
+          releaseMist();
+          delayForMist = true;
+        }else{
+          Serial.println("No motion detected");
+        }
+      }
+    }else{
+      Serial.println("Mist recently released, delaying mist");
+      countDelay++;
+    }
+    delay(5000);
   }
-  delay(5000);
+  connectWifi();
 }
 
-float getHeatIndex(){
-  float hum = dht.getHumidity();
-  float temp = dht.getTemperature();
+void getReadings(){
+  rh = dht.getHumidity();
+  temp = dht.getTemperature();
   while(dht.getStatusString()!="OK"){
-    hum = dht.getHumidity();
+    rh = dht.getHumidity();
     temp = dht.getTemperature();
   }
-  float heatInd = dht.computeHeatIndex(temp, hum, false);
+  hi = dht.computeHeatIndex(temp, rh, false);
+  sendHeatIndex(hi);
   sendTemp(temp);
-  int count = myPeriodic;
-  while(count--){
-    delay(1000);
-  }
-  sendRH(hum);
-  count = myPeriodic;
-  while(count--){
-    delay(1000);
-  }
-  sendHeatIndex(heatInd);
-  count = myPeriodic;
-  while(count--){
-    delay(1000);
-  }
-  return heatInd;
+  sendRH(rh);
 }
 
 bool getMotion(){
-  int lpCnt = 0;
+  Serial.println("Detecting motion...");  
   bool motTrue = false;
-  bool motDet = true;
-  while(lpCnt <= 30 && motDet){
+  int motDet = 0;
+  int falseCount = 0;
+  int trueCount = 0;
+  Serial.print("Detect count:\t");
+  while((trueCount + falseCount) <= 300){
     motDet = digitalRead(pirPin);
-    lpCnt++;
-    motTrue = motDet && (lpCnt>=30);
+    if(motDet==1){
+      trueCount++;
+      Serial.print("O");
+    }else{
+      falseCount++;
+      Serial.print("X");
+    }
     delay(100);
   }
+  Serial.println();
+  motTrue = trueCount > 50;
   return motTrue;
 }
 
 void connectWifi(){
-  Serial.print("Connecting to "+*MY_SSID);
-  WiFi.begin(MY_SSID, MY_PWD);
-  while(WiFi.status() != WL_CONNECTED){
+  Serial.print("Connecting...");
+  wifiMulti.addAP("PASCUAPARASAMASA", "lalalalala");
+  wifiMulti.addAP("PLDTHOMEDSL17A6F", "PLDTWIFI17A67");
+  wifiMulti.addAP("PLDTMyDSL", "pldtwifiD6346");
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
     delay(1000);
-    Serial.print(".");
+    Serial.print('.');
   }
-  Serial.println();
-  Serial.println("Connected");
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
   Serial.println();
 }
 
 void sendTemp(float temp){
   WiFiClient client;
+  Serial.print("Temperature:\t");
+  Serial.println(temp, 2);
+  Serial.println("Uploading temperature reading...");
   if(client.connect(server, 80)){
-//    Serial.println("Wifi Client connected");
+    Serial.println("Wifi Client connected");
     String postStr = apiKey;
-    postStr += "&field2=";
+    postStr += fieldTemp;
     postStr += String(temp);
     postStr += "\r\n\r\n";
     client.print("POST /update HTTP/1.1\n");
@@ -106,6 +141,16 @@ void sendTemp(float temp){
     client.print("\n\n");
     client.print(postStr);
     delay(1000);
+    Serial.println("Temperature uploaded");
+    Serial.print("Wait 15 seconds for next upload");
+    int count = myPeriodic;
+    while(count--){
+      Serial.print(".");
+      delay(1000);
+    }
+    Serial.println();
+  }else{
+    Serial.println("Upload failed");
   }
   sent++;
   client.stop();
@@ -113,10 +158,13 @@ void sendTemp(float temp){
 
 void sendRH(float rh){
   WiFiClient client;
+  Serial.print("Relative Humidity:\t");
+  Serial.println(rh, 2);
+  Serial.println("Uploading Relative Humidity...");
   if(client.connect(server, 80)){
-//    Serial.println("Wifi Client connected rh");
+    Serial.println("Wifi Client connected");
     String postStr = apiKey;
-    postStr += "&field3=";
+    postStr += fieldRH;
     postStr += String(rh);
     postStr += "\r\n\r\n";
     client.print("POST /update HTTP/1.1\n");
@@ -129,6 +177,16 @@ void sendRH(float rh){
     client.print("\n\n");
     client.print(postStr);
     delay(1000);
+    Serial.println("Relative Humidity uploaded");
+    Serial.print("Wait 15 seconds for next upload");
+    int count = myPeriodic;
+    while(count--){
+      Serial.print(".");
+      delay(1000);
+    }
+    Serial.println();
+  }else{
+    Serial.println("Upload failed");
   }
   sent++;
   client.stop();
@@ -136,10 +194,13 @@ void sendRH(float rh){
 
 void sendHeatIndex(float heatInd){
   WiFiClient client;
+  Serial.print("Heat Index:\t");
+  Serial.println(heatInd, 2);
+  Serial.println("Uploading Heat index...");
   if(client.connect(server, 80)){
-//    Serial.println("Wifi Client connected heatInd");
+    Serial.println("Wifi Client connected");
     String postStr = apiKey;
-    postStr += "&field4=";
+    postStr += fieldHI;
     postStr += String(heatInd);
     postStr += "\r\n\r\n";
     client.print("POST /update HTTP/1.1\n");
@@ -152,7 +213,31 @@ void sendHeatIndex(float heatInd){
     client.print("\n\n");
     client.print(postStr);
     delay(1000);
+    Serial.println("Heat index uploaded");
+    Serial.print("Wait 15 seconds for next upload");
+    int count = myPeriodic;
+    while(count--){
+      Serial.print(".");
+      delay(1000);
+    }
+    Serial.println();
+  }else{
+    Serial.println("Upload failed");
   }
   sent++;
   client.stop();
 }
+
+void releaseMist(){
+  Serial.println("Starting mist release sequence...");
+  int endCount = 0;
+  while(endCount < 3){
+    digitalWrite(outPin, 0);
+    delay(5000);
+    digitalWrite(outPin, 1);
+    delay(10000);
+    endCount++;
+  }
+  Serial.println("Mist release sequence has ended");
+}
+
