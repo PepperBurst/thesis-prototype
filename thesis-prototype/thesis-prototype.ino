c97f65c9cd3b5a8ae1b471c21709aa63bb48b2d1
Treestan Protector <mizzukiprime@gmail.com>
2019-05-08 20:33:41 +0800

    eksdde

--- thesis-prototype/thesis-prototype.ino
+++ thesis-prototype/thesis-prototype.ino
@@ -11,29 +11,39 @@
 
 //Read APIKEY for Channel 1 = EX1DVX903H75LZCS
 //Write APIKEY for Channel 1 = 459JH64HVXZMSZQJ
-//TalkBack ID for Channel 1 = 32023;
-//TalkBack APIKEY  = WGTNUEUDPAYLS3PN
 //Read APIKEY for Channel 2 = SR26KL05EWR7E2YZ
 //Write APIKEY for Channel 2 = N4CZSPBXEJF4U16D
 //Read APIKEY for Channel 3 = 0YG3YFK75U80P940
 //Write APIKEY for Channel 3 = XOIG3YFOHR4OK8MW
 //Read APIKEY for Channel 4 = KDIUJMWUGI9QPHPJ
 //Write APIKEY for Channel 4 = S2E20R36YWWDAMMC
+//TalkBack ID for Channel 1 = 32023;
+//TalkBack APIKEY for Channel 1 = WGTNUEUDPAYLS3PN
+//Talkback ID for channel 2 = 32823
+//TalkBack APIKEY for Channel 2  = S11XC4U836TQ0261
+//Talkback ID for channel 3 = 32824
+//TalkBack APIKEY for Channel 3  = KGUQTLB8771TD7CV
+//Talkback ID for channel 4 = 32825
+//TalkBack APIKEY for Channel 4  =  T7X7SYQN5K6Z9KK5
 
 
 const char* server = "api.thingspeak.com";
 String writeApiKey = "XOIG3YFOHR4OK8MW"; // set Write API Key
 String readApiKey = "0YG3YFK75U80P940";
-String tbApiKey = "WGTNUEUDPAYLS3PN";
+String tbApiKey = "S11XC4U836TQ0261";
+String tbID = "32824";
 int sent = 0;
 bool delayForMist = false;
 int countDelay = 0;
 String fieldHeatIndex = "&field1=";
 String fieldTemperature = "&field2=";
 String fieldRelativeHumidity = "&field3=";
-String fieldHeatIndexTreshold = "&field=4";
+String fieldHeatIndexTreshold = "&field4=";
+String fieldRelativeHumidityTreshold = "&field5=";
+String fieldSwarmSignal = "&field6=";
 String moduleName = "Module 3";     //set Module #
-const unsigned long postingInterval = 50000;
+int moduleNumber = 3;
+const unsigned long postingInterval = 200000;
 long lastUpdateTime = 0;
 float heatIndex = 0;
 float temperature = 0;
@@ -65,41 +75,22 @@ void setup() {
 
 void loop() {
   while(wifiMulti.run() == WL_CONNECTED){
-    unsigned long currentMillis = millis();
-    Serial.print("Current Time:\t");
-    Serial.println(currentMillis);
-    Serial.print("Last Update Time:\t");
-    Serial.println(lastUpdateTime);
-    if (currentMillis - lastUpdateTime >=  postingInterval) {
-      lastUpdateTime = currentMillis;
+//    Serial.println("Start Loop");
+    int loop1Count = 0;
+    if(loop1Count > 300000){
       getReadings();
       uploadReadings(heatIndex, temperature, relativeHumidity);
-      getTresholds();
-      getTalkbackCommand();
     }
+    if(heatIndex >= heatIndexTreshold && relativeHumidity <= relativeHumidityTreshold){
+      //do mist shet
+      //swarm activation
+      sendSwarmSignal(moduleNumber);
+    }
+    getTresholds();
+    getTalkbackCommand();
     delay(1000);
-//    getReadings();
-//    if(countDelay > 1){
-//      countDelay = 0;
-//      delayForMist = false;
-//    }
-//    if(!delayForMist){
-//      if(hi>32 && rh<90){
-//        Serial.println("Heat index is above threshold, Relative Humidity is in valid range");
-//        if(getMotion()){
-//          Serial.println("Motion detected, releasing mist...");
-//  //        digitalWrite(outPin, 0);
-//          releaseMist();
-//          delayForMist = true;
-//        }else{
-//          Serial.println("No motion detected");
-//        }
-//      }
-//    }else{
-//      Serial.println("Mist recently released, delaying mist");
-//      countDelay++;
-//    }
-//    delay(5000);
+    loop1Count++;
+//    Serial.println("End Loop");
   }
   connectWifi();
 }
@@ -236,11 +227,14 @@ void getRHTreshold(){
     Serial.print("Relative Humidity Treshold:\t");
     Serial.println(relativeHumidityTreshold, 2);
   }
+  client.stop();
 }
 
 void getTalkbackCommand(){
   WiFiClient client;
-  String url = "/talkbacks/32023/commands/execute?api_key=";
+  String url = "/talkbacks/";
+  url += tbID;
+  url += "/commands/execute?api_key=";
   url += tbApiKey;
   if(client.connect(server, 80)){
     client.print(String("GET "));
@@ -266,9 +260,18 @@ void getTalkbackCommand(){
       all_data += client.readStringUntil('\n');
     }
     Serial.println(all_data);
-    if(all_data=="MIST_RELEASE"){
-      releaseMist();
+    if(all_data=="MIST_RELEASE_1"){
+      releaseMist(1);
+    }else if(all_data=="MIST_RELEASE_2"){
+      releaseMist(2);
+    }else if(all_data=="MIST_RELEASE_3"){
+      releaseMist(3);
+    }else if(all_data=="MIST_RELEASE_4"){
+      releaseMist(4);
+    }else{
+      Serial.println("Default command");
     }
+    client.stop();
   }
 }
 
@@ -403,17 +406,19 @@ void sendHeatIndex(float heatInd){
   sent++;
   client.stop();
 }
-
-void sendTalkBack(String command){
-  WiFiClient client;=
-  Serial.println("Uploading command...");
+void sendSwarmSignal(int module){
+  String sourceSignal = "MIST_RELEASE_";
+  sourceSignal += String(module);
+  WiFiClient client;
+  Serial.print("Swarm Signal Source:\t");
+  Serial.println(module);
+  Serial.println("Uploading swarm signal...");
   if(client.connect(server, 80)){
     Serial.println("Wifi Client connected");
-    String postStr = tbApiKey;
-    postStr += fieldTemperature;
-    postStr += String(temp);
+    String postStr = writeApiKey;
+    postStr += fieldSwarmSignal;
+    postStr += sourceSignal;
 //    postStr += "\r\n\r\n";
-    postStr += "";
     client.print("POST /update HTTP/1.1\n");
     client.print("HOST: api.thingspeak.com\n");
     client.print("Connection: close\n");
@@ -424,7 +429,7 @@ void sendTalkBack(String command){
     client.print("\n\n");
     client.print(postStr);
     delay(1000);
-    Serial.println("Temperature uploaded");
+    Serial.println("Swarm signal uploaded");
     Serial.print("Wait 5 seconds for next upload");
     int count = myPeriodic;
     while(count--){
@@ -439,8 +444,25 @@ void sendTalkBack(String command){
   client.stop();
 }
 
-void releaseMist(){
+void releaseMist(int module){
   Serial.println("Starting mist release sequence...");
+  switch(module){
+    case 1:
+      Serial.println("Source activation module 1");
+      break;
+    case 2:
+      Serial.println("Source activation module 2");
+      break;
+    case 3:
+      Serial.println("Source activation module 3");
+      break;
+    case 4:
+      Serial.println("Source activation module 4");
+      break;
+    default:
+      Serial.println("Default flow");
+      break;
+  }
 //  int endCount = 0;
 //  while(endCount < 3){
 //    digitalWrite(outPin, 0);
